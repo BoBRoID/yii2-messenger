7caf7eaac14935a0d1c6bd1aabd03e278e54444c
BoBRoID <n.gilko@gmail.com>
2015-11-11 13:22:18 +0200

    initial commit

---
+++ Messenger.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bobroid
+ * Date: 11.11.15
+ * Time: 13:16
+ */
+
+namespace bobroid\messenger;
+
+class Messenger extends \yii\base\Widget{
+
+
+
+}
---
+++ MessengerAsset.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bobroid
+ * Date: 11.11.15
+ * Time: 13:16
+ */
+
+namespace bobroid\messenger;
+
+class MessengerAsset extends yii\web\AssetBundle{
+
+}
